6670ee06463c2fb29a9acc73ffc8407712a73e9c
Yoonkeee <yoonkeee@gmail.com>
2023-12-06 13:57:32 +0900

    eslint disable

--- frontend/src/Root.js
+++ frontend/src/Root.js
@@ -101,236 +101,201 @@ export default function Root() {
           content="initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no, viewport-fit=cover"
           name="viewport"
         />
-        />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-        <meta name="apple-mobile-web-app-title" content="쏘스윗 매니저" />
-        <link rel="shortcut icon" href="app_icon.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="app_icon.png" />
+        <meta content="yes" name="apple-mobile-web-app-capable" />
+        <meta content="black-translucent" name="apple-mobile-web-app-status-bar-style" />
+        <meta content="쏘스윗 매니저" name="apple-mobile-web-app-title" />
+        <link href="app_icon.png" rel="shortcut icon" />
+        <link href="app_icon.png" rel="apple-touch-icon" sizes="180x180" />
         {/*<link rel="apple-touch-startup-image" href="splash_screens/iPhone_14_Pro_Max_portrait.png" />*/}
         <link
           href="splash_screens/iPhone_14_Pro_Max_portrait.png"
           media="screen and (device-width: 430px) and (device-height: 932px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
           rel="apple-touch-startup-image"
         />
-        />
         <link
           href="splash_screens/iPhone_14_Pro_Max_landscape.png"
           media="screen and (device-width: 430px) and (device-height: 932px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)"
           rel="apple-touch-startup-image"
-        />
         />
         <link
           href="splash_screens/iPhone_14_Pro_landscape.png"
           media="screen and (device-width: 393px) and (device-height: 852px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)"
           rel="apple-touch-startup-image"
         />
-        />
         <link
           href="splash_screens/iPhone_14_Plus__iPhone_13_Pro_Max__iPhone_12_Pro_Max_landscape.png"
           media="screen and (device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)"
           rel="apple-touch-startup-image"
-        />
         />
         <link
           href="splash_screens/iPhone_14__iPhone_13_Pro__iPhone_13__iPhone_12_Pro__iPhone_12_landscape.png"
           media="screen and (device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)"
           rel="apple-touch-startup-image"
         />
-        />
         <link
           href="splash_screens/iPhone_13_mini__iPhone_12_mini__iPhone_11_Pro__iPhone_XS__iPhone_X_landscape.png"
           media="screen and (device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)"
           rel="apple-touch-startup-image"
-        />
         />
         <link
           href="splash_screens/iPhone_11_Pro_Max__iPhone_XS_Max_landscape.png"
           media="screen and (device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)"
           rel="apple-touch-startup-image"
         />
-        />
         <link
           href="splash_screens/iPhone_11__iPhone_XR_landscape.png"
           media="screen and (device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
           rel="apple-touch-startup-image"
-        />
         />
         <link
           href="splash_screens/iPhone_8_Plus__iPhone_7_Plus__iPhone_6s_Plus__iPhone_6_Plus_landscape.png"
           media="screen and (device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)"
           rel="apple-touch-startup-image"
         />
-        />
         <link
           href="splash_screens/iPhone_8__iPhone_7__iPhone_6s__iPhone_6__4.7__iPhone_SE_landscape.png"
           media="screen and (device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
           rel="apple-touch-startup-image"
-        />
         />
         <link
           href="splash_screens/4__iPhone_SE__iPod_touch_5th_generation_and_later_landscape.png"
           media="screen and (device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
           rel="apple-touch-startup-image"
         />
-        />
         <link
           href="splash_screens/12.9__iPad_Pro_landscape.png"
           media="screen and (device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
           rel="apple-touch-startup-image"
-        />
         />
         <link
           href="splash_screens/11__iPad_Pro__10.5__iPad_Pro_landscape.png"
           media="screen and (device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
           rel="apple-touch-startup-image"
         />
-        />
         <link
           href="splash_screens/10.9__iPad_Air_landscape.png"
           media="screen and (device-width: 820px) and (device-height: 1180px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
           rel="apple-touch-startup-image"
-        />
         />
         <link
           href="splash_screens/10.5__iPad_Air_landscape.png"
           media="screen and (device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
           rel="apple-touch-startup-image"
         />
-        />
         <link
           href="splash_screens/10.2__iPad_landscape.png"
           media="screen and (device-width: 810px) and (device-height: 1080px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
           rel="apple-touch-startup-image"
-        />
         />
         <link
           href="splash_screens/9.7__iPad_Pro__7.9__iPad_mini__9.7__iPad_Air__9.7__iPad_landscape.png"
           media="screen and (device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
           rel="apple-touch-startup-image"
         />
-        />
         <link
           href="splash_screens/8.3__iPad_Mini_landscape.png"
           media="screen and (device-width: 744px) and (device-height: 1133px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
           rel="apple-touch-startup-image"
-        />
         />
         <link
           href="splash_screens/iPhone_14_Pro_portrait.png"
           media="screen and (device-width: 393px) and (device-height: 852px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
           rel="apple-touch-startup-image"
         />
-        />
         <link
           href="splash_screens/iPhone_14_Plus__iPhone_13_Pro_Max__iPhone_12_Pro_Max_portrait.png"
           media="screen and (device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
           rel="apple-touch-startup-image"
-        />
         />
         <link
           href="splash_screens/iPhone_14__iPhone_13_Pro__iPhone_13__iPhone_12_Pro__iPhone_12_portrait.png"
           media="screen and (device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
           rel="apple-touch-startup-image"
         />
-        />
         <link
           href="splash_screens/iPhone_13_mini__iPhone_12_mini__iPhone_11_Pro__iPhone_XS__iPhone_X_portrait.png"
           media="screen and (device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
           rel="apple-touch-startup-image"
-        />
         />
         <link
           href="splash_screens/iPhone_11_Pro_Max__iPhone_XS_Max_portrait.png"
           media="screen and (device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
           rel="apple-touch-startup-image"
         />
-        />
         <link
           href="splash_screens/iPhone_11__iPhone_XR_portrait.png"
           media="screen and (device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
           rel="apple-touch-startup-image"
-        />
         />
         <link
           href="splash_screens/iPhone_8_Plus__iPhone_7_Plus__iPhone_6s_Plus__iPhone_6_Plus_portrait.png"
           media="screen and (device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
           rel="apple-touch-startup-image"
         />
-        />
         <link
           href="splash_screens/iPhone_8__iPhone_7__iPhone_6s__iPhone_6__4.7__iPhone_SE_portrait.png"
           media="screen and (device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
           rel="apple-touch-startup-image"
-        />
         />
         <link
           href="splash_screens/4__iPhone_SE__iPod_touch_5th_generation_and_later_portrait.png"
           media="screen and (device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
           rel="apple-touch-startup-image"
         />
-        />
         <link
           href="splash_screens/12.9__iPad_Pro_portrait.png"
           media="screen and (device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
           rel="apple-touch-startup-image"
-        />
         />
         <link
           href="splash_screens/11__iPad_Pro__10.5__iPad_Pro_portrait.png"
           media="screen and (device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
           rel="apple-touch-startup-image"
         />
-        />
         <link
           href="splash_screens/10.9__iPad_Air_portrait.png"
           media="screen and (device-width: 820px) and (device-height: 1180px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
           rel="apple-touch-startup-image"
-        />
         />
         <link
           href="splash_screens/10.5__iPad_Air_portrait.png"
           media="screen and (device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
           rel="apple-touch-startup-image"
         />
-        />
         <link
           href="splash_screens/10.2__iPad_portrait.png"
           media="screen and (device-width: 810px) and (device-height: 1080px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
           rel="apple-touch-startup-image"
-        />
         />
         <link
           href="splash_screens/9.7__iPad_Pro__7.9__iPad_mini__9.7__iPad_Air__9.7__iPad_portrait.png"
           media="screen and (device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
           rel="apple-touch-startup-image"
         />
-        />
         <link
           href="splash_screens/8.3__iPad_Mini_portrait.png"
           media="screen and (device-width: 744px) and (device-height: 1133px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
           rel="apple-touch-startup-image"
         />
-        />
       </Helmet>
-      <Box m={'0'} overflow={'hidden'} style={{ fontFamily: 'GmarketSans' }} bgColor={mainColor} ref={div}>
+      <Box bgColor={mainColor} m={'0'} overflow={'hidden'} ref={div} style={{ fontFamily: 'GmarketSans' }}>
         <Box bgColor={mainColor} h={'10vh'} position="fixed" top={0} w={'100%'} zIndex={1}>
           <Box
-            h={statusBarHeight}
             bgColor={mainColor}
-            textAlign={'center'}
+            h={statusBarHeight}
             justifyContent={'center'}
+            textAlign={'center'}
             zIndex={55}
           />
           <Header />
         </Box>
-        <Box overflowX={'hidden'} bgColor={'white'} zIndex={54}>
+        <Box bgColor={'white'} overflowX={'hidden'} zIndex={54}>
           <Box
-            h={`calc(10vh + ${statusBarHeight})`}
-            display={'flex'}
-            bgColor={mainColor}
-            justifyContent={'center'}
             alignItems={'flex-end'}
+            bgColor={mainColor}
+            display={'flex'}
+            h={`calc(10vh + ${statusBarHeight})`}
+            justifyContent={'center'}
             zIndex={55}
           >
             <HStack alignItems={'flex-end'}>
@@ -351,7 +316,7 @@ export default function Root() {
             {/*    새로고침*/}
             {/*</Text>*/}
           </Box>
-          <Outlet overflowX={'hidden'} h={`calc(80vh + ${statusBarHeight})`} zIndex={56} />
+          <Outlet h={`calc(80vh + ${statusBarHeight})`} overflowX={'hidden'} zIndex={56} />
         </Box>
         <Box bottom={0} h={'10vh'} position="fixed" w={'100%'} zIndex={0}>
           <Footer />
